44618c3bbbd0921cac2dc611494a445b8d007c8b
limjunho <jewel7492@gmail.com>
2023-03-27 12:36:49 +0900

    [UPDATE] allow all outgoing traffic

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -56,6 +56,13 @@ resource "aws_security_group" "security_group" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_route_table_association" "route_table_association" {
