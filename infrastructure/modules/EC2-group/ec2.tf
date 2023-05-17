c35b2ddd8ca7ca3a9c1dba7ddb1f6dffcc83326b
limjunho <jewel7492@gmail.com>
2023-05-17 12:54:29 +0900

    [FIX] Increase instance creation waiting time to prevent EFS mount failure (30s -> 60s)

--- infrastructure/modules/EC2-group/ec2.tf
+++ infrastructure/modules/EC2-group/ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2" {
 
   user_data = <<-EOF
             #!/bin/bash
-            sleep 30
+            sleep 60
             mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${var.efs_dns_name}:/ /home/ubuntu/migration_test/dump
             sudo chown ubuntu:ubuntu /home/ubuntu/migration_test/dump
             sudo timedatectl set-timezone 'Asia/Seoul'
