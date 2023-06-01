a071e918e6d81aa882645826f21e1b10806846e6
limjunho <jewel7492@gmail.com>
2023-06-01 15:41:40 +0900

    [FIX] typo correction

--- data_processing_for_lscpu/modules/CPUFeatures_h.py
+++ data_processing_for_lscpu/modules/CPUFeatures_h.py
@@ -10,7 +10,7 @@ def all_flags():
     '''
     # Intel
     CPUID_1_EDX = ['fpu', 'vme', 'de', 'pse', 'tsc', 'msr', 'pae', 'mce', 'cx8', 'apic', 'sep', 'mtrr', 'pge', 'mca', 'cmov', 'pat', 'pse36', 'pn', 'clflush', 'dts', 'acpi', 'mmx', 'fxsr', 'sse', 'sse2', 'ss', 'ht', 'tm', 'ia64', 'pbe']
-    CPUID_1_ECX = ['pni', 'pclmulqdq', 'dtes64', 'monitor', 'ds_cpl', 'vmx', 'smx', 'est', 'tm2', 'ssse3', 'cid', 'sdbg', 'fma', 'cx116', 'xtpr', 'pdcm', 'pcid', 'dca', 'sse4_1', 'sse4_2', 'x2apic', 'movbe', 'popcnt', 'tsc_deadline_timer', 'aes', 'xsave', 'osxsave', 'avx', 'f16c', 'rdrand', 'hypervisor']
+    CPUID_1_ECX = ['pni', 'pclmulqdq', 'dtes64', 'monitor', 'ds_cpl', 'vmx', 'smx', 'est', 'tm2', 'ssse3', 'cid', 'sdbg', 'fma', 'cx16', 'xtpr', 'pdcm', 'pcid', 'dca', 'sse4_1', 'sse4_2', 'x2apic', 'movbe', 'popcnt', 'tsc_deadline_timer', 'aes', 'xsave', 'osxsave', 'avx', 'f16c', 'rdrand', 'hypervisor']
     CPUID_7_0_EBX = ['fsgsbase', 'tsc_adjust', 'sgx', 'bmi1', 'hle', 'avx2', 'fdp_excptn_only', 'smep', 'bmi2', 'erms', 'invpcid', 'rtm', 'cqm', 'zero_fcs_fds', 'mpx', 'rdt_a', 'avx512f', 'avx512dq', 'rdseed', 'adx', 'smap', 'avx512ifma', 'clflushopt', 'clwb', 'intel_pt', 'avx512pf', 'avx512er', 'avx512cd', 'sha_ni', 'avx512bw', 'avx512vl']
     CPUID_7_1_EAX = ['avx_vnni', 'avx512_bf16', 'cmpccxadd', 'arch_perfmon_ext', 'fzrm', 'fsrs', 'fsrc', 'lkgs', 'amx_fp16', 'avx_ifma']
     CPUID_7_0_ECX = ['avx512vbmi', 'umip', 'pku', 'ospke', 'waitpkg', 'avx512_vbmi2', 'gfni', 'vaes', 'vpclmulqdq', 'avx512_vnni', 'avx512_bitalg', 'tme', 'avx512_vpopcntdq', 'la57', 'rdpid', 'bus_lock_detect', 'cldemote', 'movdiri', 'movdir64b', 'enqcmd', 'sgx_lc']
