405f025a08196c144da58e87a528157c2843f201
limjunho <jewel7492@gmail.com>
2023-06-13 14:20:02 +0900

    [UPDATE] Update the number of groups

--- data_processing_for_lscpu/entire/SupersetVisualization(Digraph).py
+++ data_processing_for_lscpu/entire/SupersetVisualization(Digraph).py
@@ -11,5 +11,5 @@ df = GspreadUtils.read_CPU_Feature_Visualization('simplized aws group(all)')
 featureGroups = df['feature groups'].tolist()
 df = df.drop('feature groups', axis=1)
 
-GROUP_NUMBER = 37
+GROUP_NUMBER = 31
 Transferable.Digraph(GROUP_NUMBER, df)
