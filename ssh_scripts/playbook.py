6ecd74ef4b9bd7cb1c1b6eecaf807ff1acd4f430
limjunho <jewel7492@gmail.com>
2024-08-23 14:35:17 +0900

    [ADD] python random number generation

--- ssh_scripts/playbook.py
+++ ssh_scripts/playbook.py
@@ -12,10 +12,10 @@ def setWorkload():
     global USER
 
     workloads = ['matrix_multiplication', 'redis', 'ubuntu_container', 'xgboost', 'rubin', 'c_matrix_multiplication', 'cpp_xgboost', 'adox_adcx',
-                 'pku', 'rdseed', 'sha', 'criu_cpu_check']
+                 'pku', 'rdseed', 'sha', 'criu_cpu_check', 'py_rdseed']
     print('Select workloads to experiment with')
     print(f'1. {workloads[0]}\n2. {workloads[1]}\n3. {workloads[2]}\n4. {workloads[3]}\n5. {workloads[4]}\n6. {workloads[5]}\n7. {workloads[6]}')
-    print(f'8. {workloads[7]}\n9. {workloads[8]}\n10. {workloads[9]}\n11. {workloads[10]}\n12. {workloads[11]}')
+    print(f'8. {workloads[7]}\n9. {workloads[8]}\n10. {workloads[9]}\n11. {workloads[10]}\n12. {workloads[11]}\n13. {workloads[12]}')
     
     index = int(input()) - 1
 
@@ -112,10 +112,39 @@ def funcTracking(groups, is_lazy_binding=False):
     with open(f'ansible.log', 'w') as f:
         subprocess.run(["ansible-playbook", f"ssh_scripts/{WORKLOAD}/{playbook}.yml",
                        "-i", "ssh_scripts/inventory.json", "--forks", f"{len(groups)}"], stdout=f, stderr=f)
-        
+
+def bytecodeTracking(groups):
+    if not os.path.exists(f'ssh_scripts/{WORKLOAD}/bytecode_tracking.yml'):
+        return
+    
+    sources = []
+
+    for i in range(len(groups)):
+        with open("ssh_scripts/inventory_" + str(groups[i]) + ".txt") as f:
+            source = f.readlines()
+        sources += [src.strip() for src in source]
+
+    inventory = {
+        "all": {
+            "vars": {
+                "ansible_user": f"{USER}",
+                "ansible_ssh_common_args": "-o 'StrictHostKeyChecking=no'",
+            },
+            "hosts": {src: None for src in sources}
+        },
+    }
+
+    # Update dynamic inventory file
+    with open("ssh_scripts/inventory.json", "w") as f:
+        json.dump(inventory, f)
+
+    with open(f'ansible.log', 'w') as f:
+        subprocess.run(["ansible-playbook", f"ssh_scripts/{WORKLOAD}/bytecode_tracking.yml",
+                       "-i", "ssh_scripts/inventory.json", "--forks", f"{len(groups)}"], stdout=f, stderr=f)
+
 
 def entire_scanning(groups):
-    if not os.path.exists('ssh_scripts/entire_scanning/entire_scanning.yml'):
+    if not os.path.exists(f'ssh_scripts/{WORKLOAD}/entire_scanning.yml'):
         return
     
     sources = []
@@ -140,7 +169,7 @@ def entire_scanning(groups):
         json.dump(inventory, f)
 
     with open(f'ansible.log', 'w') as f:
-        subprocess.run(["ansible-playbook", f"ssh_scripts/entire_scanning/entire_scanning.yml",
+        subprocess.run(["ansible-playbook", f"ssh_scripts/{WORKLOAD}/entire_scanning.yml",
                        "-i", "ssh_scripts/inventory.json", "--forks", f"{len(groups)}"], stdout=f, stderr=f)
 
 
